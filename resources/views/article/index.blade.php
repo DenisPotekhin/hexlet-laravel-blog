1221f5bfd1db3b95966431ef2c353aa747691857
Denis <potekhin@umgoroda.ru>
2019-10-20 16:36:11 +0200

    fifteenth commit

--- resources/views/article/index.blade.php
+++ resources/views/article/index.blade.php
@@ -4,6 +4,7 @@
     <h1>Список статей</h1>
     @foreach($articles as $article)
         <h2><a href="/articles/{{$article->id}}">{{$article->name}}</a></h2>
+        <small><a href="/articles/{{$article->id}}/edit"> редактировать статью</a></small>
         {{-- Str::limit – функция-хелпер, которая обрезает текст до указанной длины --}}
         {{-- Используется для очень длинных текстов, которые нужно сократить --}}
         <div>{{Str::limit($article->body, 200)}}</div>
